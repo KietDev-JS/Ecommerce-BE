bfa28a5f6b4b5a6d7cd3cd89a94db2bbf68548ec
KietDev-JS <KietPersonalMail@gmail.com>
2024-01-05 19:23:29 +0700

    remove add new button in products

--- app/dashboard/(routes)/products/components/client.tsx
+++ app/dashboard/(routes)/products/components/client.tsx
@@ -1,9 +1,5 @@
 "use client";
-
-import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
-
-import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/data-table";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
@@ -26,9 +22,6 @@ export const ProductsClient: React.FC<ProductsClientProps> = ({ data }) => {
           title={`Products (${data.length})`}
           description="Manage products for your store"
         />
-        <Button onClick={() => router.push(`/dashboard/products/new`)}>
-          <Plus className="mr-2 h-4 w-4" /> Add New
-        </Button>
       </div>
       <Separator />
       <DataTable searchKey="name" columns={columns} data={data} />
